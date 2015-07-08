8a858ea2aeba6b0c9ed0d70ef8b32407355be362
yogeshKulkarni035 <kulkarni.yogesh.228@gmail.com>
2015-07-09 00:14:25 +0530

    Initial Commit

---
+++ src/main/java/com/sampleMavenApp/App.java
@@ -0,0 +1,13 @@
+package com.sampleMavenApp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
